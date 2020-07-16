572b7065c96da99ec8ba2828057b92d04bd81093
Schikan <schikan@yahoo.co.uk>
2020-07-16 20:49:07 +0100

    next commit

---
+++ script.js
@@ -0,0 +1,9 @@
+/* SCRIPTS.JS */
+
+function changePic() {
+    document.getElementById("profilePic").src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian-prp.jpg";
+  }
+  
+  function normalPic() {
+    document.getElementById("profilePic") .src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/594589/adrian.webp";
+  }
